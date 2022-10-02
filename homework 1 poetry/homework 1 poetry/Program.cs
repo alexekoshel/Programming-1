8f8c8b939d5099265090c1c2de197fe3b1a74023
alexey koshelev <koshelevlexa2015@yandex.ru>
2022-10-02 13:28:07 +0500

    This is my homework nymber 1

---
+++ homework 1 poetry/homework 1 poetry/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_1_poetry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Федор Тютчев\r\n");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("О, как убийственно мы любим\r\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("О, как убийственно мы любим," +
+                "\r\nКак в буйной слепоте страстей" +
+                "\r\nМы то всего вернее губим," +
+                "\r\nЧто сердцу нашему милей!" +
+                "\r\n\r\nДавно ль, гордясь своей победой," +
+                "\r\nТы говорил: она моя..." +
+                "\r\nГод не прошел - спроси и сведай," +
+                "\r\nЧто уцелело от нея?");
+        }
+    }
+}
